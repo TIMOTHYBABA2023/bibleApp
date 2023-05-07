e60b36b4b760944fa196fc9ecb4e271f3d3e057a
TIMOTHYBABA2023 <timothybaba01@gmail.com>
2023-05-07 17:04:35 +0100

    bibleBooksVerse.html

---
+++ bibleBooksVerse.js
@@ -0,0 +1,42 @@
+const bibleBook = [[
+    'Genesis', 'Exodus', 'Leviticus', 'Numbers', 'Deuteronomy',
+    'Joshua', 'Judges', 'Ruth', '1 Samuel', '2 Samuel',
+    '1 Kings', '2 Kings', '1 Chronicles', '2 Chronicles',
+    'Ezra', 'Nehemiah', 'Esther', 'Job', 'Psalm', 'Proverbs',
+    'Ecclesiastes', 'Song of Solomon', 'Isaiah', 'Jeremiah',
+    'Lamentations', 'Ezekiel', 'Daniel', 'Hosea', 'Joel',
+    'Amos', 'Obadiah', 'Jonah', 'Micah', 'Nahum', 'Habakkuk',
+    'Zephaniah', 'Haggai', 'Zechariah', 'Malachi'],
+     [ 
+    'Matthew', 'Mark', 'Luke', 'John', 'Acts', 'Romans', '1 Corinthians', '2 Corinthians', 'Galatians',
+    'Ephesians', 'Philippians', 'Colossians', '1 Thessalonians', '2 Thessalonians', '1 Timothy',
+    '2 Timothy', 'Titus', 'Philemon', 'Hebrews', 'James', '1 Peter', '2 Peter', '1 John', '2 John',
+    '3 John', 'Jude', 'Revelation'
+]];
+
+const bibleVerses = [
+
+'1. Blessed are the undefiled in the way, who walk in the law of the Lord.',
+'2. Blessed are they that keep his testimonies, and that seek him with the whole heart.',
+'3. They also do no iniquity: they walk in his ways.',
+'4. Thou hast commanded us to keep thy precepts diligently.',
+'5. O that my ways were directed to keep thy statutes!',
+'6. Then shall I not be ashamed, when I have respect unto all thy commandments.',
+'7. I will praise thee with uprightness of heart, when I shall have learned thy righteous judgments.',
+'8. I will keep thy statutes: O forsake me not utterly.',
+'9. Wherewithal shall a young man cleanse his way? by taking heed thereto according to thy word.',
+'10. With my whole heart have I sought thee: O let me not wander from thy commandments.',
+'11. Thy word have I hid in mine heart, that I might not sin against thee.',
+'12. Blessed art thou, O Lord: teach me thy statutes.',
+'13. With my lips have I declared all the judgments of thy mouth.',
+'14. I have rejoiced in the way of thy testimonies, as much as in all riches.',
+'15. I will meditate in thy precepts, and have respect unto thy ways.',
+'16. I will delight myself in thy statutes: I will not forget thy word.',
+'17. Deal bountifully with thy servant, that I may live, and keep thy word.',
+'18. Open thou mine eyes, that I may behold wondrous things out of thy law.',
+'19. I am a stranger in the earth: hide not thy commandments from me.',
+'20. My soul breaketh for the longing that it hath unto thy judgments at all times.',
+];
+
+
+export {bibleBook,bibleVerses};
